e057bd440f065c45984e035788416c448082ec21
hyebyjun <hyebyjun@gmail.com>
2022-06-21 19:49:31 +0900

    post 요청으로 몽고DB에 저장하기

--- projects/movie/app.py
+++ projects/movie/app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient(
+    'mongodb+srv://test:sparta@cluster0.rf8ug.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
 app = Flask(__name__)
 
 
@@ -9,9 +17,28 @@ def home():
 
 @app.route("/movie", methods=["POST"])
 def movie_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')['content']
+    image = soup.select_one('meta[property="og:image"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
+
+    doc = {
+        'title': title,
+        'image': image,
+        'desc': desc,
+        'star': star_receive,
+        'comment': comment_receive
+    }
+    db.movies.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
 
 
 @app.route("/movie", methods=["GET"])
