7d62c8e20005f12fad21339ccb421b148437cd29
hyebyjun <hyebyjun@gmail.com>
2022-07-09 22:34:23 +0900

    localDB 연결, 데이터 통신 체크

--- sparta_plus/project01/app.py
+++ sparta_plus/project01/app.py
@@ -5,9 +5,8 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.dbsparta_plus_week1
 
 
 # HTML을 주는 부분
@@ -18,16 +17,21 @@ def home():
 
 @app.route('/diary', methods=['GET'])
 def show_diary():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg': 'GET 연결 완료!'})
+    diaries = list(db.diary.find({}, {'_id': False}))
+    return jsonify({'all_diary': diaries})
 
 
 @app.route('/diary', methods=['POST'])
 def save_diary():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 요청 완료!'})
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+
+    doc = {
+        'title': title_receive,
+        'content': content_receive
+    }
+    db.diary.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
 
 
 if __name__ == '__main__':
