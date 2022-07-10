c28a640eb94a4c5850b04aeed7dca6926416b550
hyebyjun <hyebyjun@gmail.com>
2022-07-10 19:33:43 +0900

    mongoDB 클라우드로 연결, 저장 시 날짜 표기 기능 추가 후 배포

--- sparta_plus/project01/app.py
+++ sparta_plus/project01/app.py
@@ -7,8 +7,11 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-db = client.dbsparta_plus_week1
+# client = MongoClient('localhost', 27017)
+client = MongoClient(
+    'mongodb+srv://test:sparta@cluster0.rf8ug.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+# db = client.dbsparta_plus_week1
 
 
 # HTML을 주는 부분
@@ -29,11 +32,12 @@ def save_diary():
     content_receive = request.form['content_give']
     file = request.files["file_give"]
 
-    extension = file.filename.split('.')[-1] # 저장된 파일이름에서 확장자명 따오기
+    extension = file.filename.split('.')[-1]  # 저장된 파일이름에서 확장자명 따오기
 
     # 파일 저장 시에 파일이름을 저장시각으로 정해주기
     today = datetime.now()
     mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    created_date = today.strftime('%Y-%m-%d')
     filename = f'file-{mytime}'
 
     save_to = f'static/{filename}.{extension}'
@@ -42,7 +46,8 @@ def save_diary():
     doc = {
         'title': title_receive,
         'content': content_receive,
-        'file': f'{filename}.{extension}'
+        'file': f'{filename}.{extension}',
+        'date': created_date
     }
     db.diary.insert_one(doc)
     return jsonify({'msg': '저장 완료!'})
